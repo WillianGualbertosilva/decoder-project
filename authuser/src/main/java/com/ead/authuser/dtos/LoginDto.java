f84b8d398fa2a29b6948464ea67a13e7861a268f
Willian <willian.java@gmail.com>
2024-01-05 19:46:09 -0300

    projeto do curso comunicacao entre microservicos sincrono/assicrono utilizando as boas práticas de design microservices

---
+++ authuser/src/main/java/com/ead/authuser/dtos/LoginDto.java
@@ -0,0 +1,14 @@
+package com.ead.authuser.dtos;
+
+import lombok.Data;
+import jakarta.validation.constraints.NotBlank;
+
+@Data
+public class LoginDto {
+
+    @NotBlank
+    private String username;
+    @NotBlank
+    private String password;
+
+}
---
+++ course/src/main/java/com/ead/course/enums/Month.java
@@ -0,0 +1,16 @@
+package com.ead.course.enums;
+
+public enum Month {
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER;
+}
